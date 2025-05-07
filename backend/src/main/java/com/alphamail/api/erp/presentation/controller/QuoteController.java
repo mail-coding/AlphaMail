5675ba8668c65ba7a6f2503762adfed5f21608d5
unknown <yoonji0624@naver.com>
2025-05-07 15:43:39 +0900

    [S12P31A601-167] :sparkles: feat: 견적서 상세조회 기능을 개발하라

--- backend/src/main/java/com/alphamail/api/erp/presentation/controller/QuoteController.java
+++ backend/src/main/java/com/alphamail/api/erp/presentation/controller/QuoteController.java
@@ -2,13 +2,17 @@ package com.alphamail.api.erp.presentation.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alphamail.api.erp.application.dto.RegistResultDto;
+import com.alphamail.api.erp.application.usecase.quote.GetQuoteUseCase;
 import com.alphamail.api.erp.application.usecase.quote.RegistQuoteUseCase;
+import com.alphamail.api.erp.presentation.dto.quote.GetQuoteResponse;
 import com.alphamail.api.erp.presentation.dto.quote.RegistQuoteRequest;
 import com.alphamail.api.global.dto.RegistErpResponse;
 import com.alphamail.common.constants.ApiPaths;
@@ -20,7 +24,19 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping(ApiPaths.ERP_BASE_API)
 public class QuoteController {
 
+	private final GetQuoteUseCase getQuoteUseCase;
 	private final RegistQuoteUseCase registQuoteUseCase;
+
+	@GetMapping(ApiPaths.QUOTES_BASE_API + "/{quoteId}")
+	public ResponseEntity<GetQuoteResponse> getQuote(@PathVariable Integer quoteId) {
+		GetQuoteResponse response = getQuoteUseCase.execute(quoteId);
+
+		if (response == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+
+		return ResponseEntity.ok(response);
+	}
 
 	@PostMapping(ApiPaths.QUOTES_BASE_API)
 	public ResponseEntity<?> regist(@RequestBody RegistQuoteRequest request) {
