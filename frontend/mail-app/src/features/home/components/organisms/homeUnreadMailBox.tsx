aac75ad8f0a1f8ae3d246aeda1f41794ba7b4022
maintaein <daumdaum005@gmail.com>
2026-04-15 20:11:57 +0900

    fix: 메인 대시보드 업데이트를 개선하라

--- frontend/mail-app/src/features/home/components/organisms/homeUnreadMailBox.tsx
+++ frontend/mail-app/src/features/home/components/organisms/homeUnreadMailBox.tsx
@@ -77,15 +77,6 @@ export const HomeUnreadMailBox: React.FC = () => {
   }, [location.pathname, refetch]);
 
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      console.log('안읽은 메일 데이터 갱신');
-      refetch();
-    }, 10000); // 10초마다 갱신
-    
-    return () => clearInterval(intervalId);
-  }, [refetch]);
-  
   // 메일 읽음 상태 변경 감지
   useEffect(() => {
     // 메일 읽음 상태 변경 이벤트 구독
